7e7b4236abeec54e8e57708b6aa43ea0987bd78b
AndrewPivots <44641894+AndrewPivots@users.noreply.github.com>
2019-01-27 08:33:30 -0700

    First Project Submit
    I covered the basics for the rubric here.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Classic Arcade Game Clone Project
+
+## Table of Contents
+
+- How To Play
+- Future Development
+
+## Instructions
+
+1. Open index.html in a browser
+2. Control player with keyboard arrows
+3. Avoid bugs and make it to the water to win!
+
+## Future Development
+
+1. Figure out how to randomize the bug speeds and locations
+2. Incorporate "dt" with speed.
+3. Add enemy objects dynamically enemy group array
+4. Research better way to initiate enemy objects? Feels clunky.
---
+++ js/app.js
@@ -0,0 +1,101 @@
+//aa every global variable lessens performance?
+const borderL = -100;
+const borderR = 510;
+
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    //aa set location and speed
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    //aa sets the speed based on location, sets borders, handles collisions, reset at win
+    this.x += this.speed;
+    if(this.x > borderR){
+      this.x = borderL;
+     }
+    if (this.x == player.x && this.y == player.y){
+      player.x = 200;
+      player.y = 375;
+      window.alert('BUG ATTACK! Start over.');
+    } if (player.y < 0){
+      window.setTimeout(window.alert,50,'The SPLASH of success!');
+      player.x = 200;
+      player.y = 375;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+// aa below most
+var Player = function() {
+    this.x = 200;
+    this.y = 375;
+    this.sprite = 'images/char-boy.png';
+}
+Player.prototype.update = function(){
+  // code
+}
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function(allowedKeys){
+  if(allowedKeys == 'left' && this.x > 0){
+    this.x -= 100;
+  } else if (allowedKeys == 'right' && this.x < 400){
+    this.x += 100;
+  } else if (allowedKeys == 'down' && this.y < 375){
+    this.y += 80;
+  } else if (allowedKeys == 'up' && this.y > -25){
+    this.y -= 80;
+  } else {
+    //window.alert('Ouch!');
+  }
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+//aa creates objects
+let enemy1 = new Enemy(borderL, 55, 4); // how do I decide how many to create?
+let enemy2 = new Enemy(borderL, 135, 10);
+let enemy3 = new Enemy(borderL, 215, 5); // has to be devisible by the player's coordinates
+let allEnemies = [enemy1, enemy2, enemy3]; // how to cycle through all enemy objects dynamically?
+var player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
